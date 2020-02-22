92854ecd8bd0f0d42434e198377ad1bde2bd583a
kakonose0322 <3072994091@qq.com>
2020-02-22 16:54:28 +0800

    修改网络地址

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export function request(config) {
     // 1.创建axios实例
     const instance = axios.create({
-      baseURL: 'http://106.54.54.237:8000/api/v1',
+      baseURL: 'http://106.54.54.237:8000/api/hy',
       timeout: 5000
     })
   // 2.axios的拦截器
